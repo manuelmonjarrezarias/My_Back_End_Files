d90945d2f3d8b8d71270a3aec1e0801e9291e790
MnjarrezArias <mnuel0604@gmail.com>
2020-11-02 11:16:40 -0600

    Update connecting.cs

--- connecting.cs
+++ connecting.cs
@@ -1,1 +1,12 @@
 
+import from https://github.com/manuelmonjarrezarias/My_Back_End_Files/.* as mainFailov;
+
+public class Connection{
+  
+  var connect;
+  var query;
+  var temp;
+
+  var i, j, n, x, y;
+
+}
