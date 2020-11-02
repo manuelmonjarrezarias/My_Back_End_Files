d49d334649b33eb15a9ddf3e5e9c406c6f701a36
MnjarrezArias <mnuel0604@gmail.com>
2020-11-02 11:15:24 -0600

    Create render.php

---
+++ render.php
@@ -0,0 +1,11 @@
+import from https://github.com/manuelmonjarrezarias/My_Back_End_Files/.* as mainFailov;
+
+public class Connection{
+  
+  var connect;
+  var query;
+  var temp;
+
+  var i, j, n, x, y;
+
+}
